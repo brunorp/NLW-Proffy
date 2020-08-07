cb7dc652ad8a37f7ff962f7d2cd9f7bee4ba7864
brunorp <bruno.rossetto97@gmail.com>
2020-08-07 20:24:44 -0300

    connections

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -9,6 +9,7 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string; // obrigatória
+    description?: string;
     //title?: string; opcional
 }
 
@@ -24,6 +25,7 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p> }
                 {props.children}
             </div>
 
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,49 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', { 
+            user_id: teacher.id
+        });
+    }
     return(
         <article className="teacher-item"> 
             <header>
-                <img src="https://images.pexels.com/photos/3408420/pexels-photo-3408420.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>João Vasconcelos</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <br /> <br />
-            Nunc porttitor massa ut nulla vulputate, a efficitur nulla posuere. Curabitur at mi ornare, scelerisque velit ut, commodo erat. 
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 40,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
